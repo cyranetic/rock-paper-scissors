bf10881ec20133e884aa8c76783b10dd753583d3
Sara Segel <sara.segel@gmail.com>
2023-07-13 19:34:24 +0900

    get playGame function to work with single game

--- script.js
+++ script.js
@@ -18,19 +18,51 @@ function getComputerChoice() {
   return computerSelection;
 }
 
-getComputerChoice();
+let computerSelection = getComputerChoice();
+
+//use prompt() to get input from user
+let playerSelection = prompt("Rock, paper, or scissors?");
+//playerSelection parameter should be case-insensitive, change user input to all lowercase to be able to match a computerSelection choice
+playerSelection = playerSelection.toLowerCase();
+console.log(playerSelection);
 
 function playGame(playerSelection, computerSelection) {
-  //use prompt() to get input from user
   //return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-  //playerSelection parameter should be case-insensitive, change user input to all lowercase to be able to match a computerSelection choice
   //write conditional that determines winner
-  function game() {
-    //goes inside playGame function
-    //has 5 rounds
-    //console.log results after each round
-    //keeps score
-    //reports winner or loser at end
-    //console.log winner at end
+  if (playerSelection === "rock") {
+    if (computerSelection === "rock") {
+      console.log(`It's a tie!`);
+    } else if (computerSelection === "paper") {
+      console.log("Computer plays paper. Computer wins!");
+    } else if (computerSelection === "scissors") {
+      console.log("Computer plays scissors. You win!");
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "paper") {
+      console.log(`It's a tie!`);
+    } else if (computerSelection === "scissors") {
+      console.log("Computer plays scissors. Computer wins!");
+    } else if (computerSelection === "rock") {
+      console.log("Computer plays rock. You win!");
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "scissors") {
+      console.log(`It's a tie!`);
+    } else if (computerSelection === "rock") {
+      console.log("Computer plays rock. Computer wins!");
+    } else if (computerSelection === "paper") {
+      console.log("Computer plays paper. You win!");
+    }
   }
+}
+
+playGame(playerSelection, computerSelection);
+
+function game() {
+  //goes inside playGame function
+  //has 5 rounds
+  //console.log results after each round
+  //keeps score
+  //reports winner or loser at end
+  //console.log winner at end
 }
