aac9bc68e64ff5ef5f002b423fe64b2d91c0946d
Sara Segel <sara.segel@gmail.com>
2023-07-13 21:59:04 +0900

    get full working game functionality

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-console.log("Hello world!!");
+//initialize variables
+let computerSelection;
+let playerSelection;
+let scores = {
+  computerScore: 0,
+  playerScore: 0,
+};
 
-function getComputerChoice() {
+function getComputerSelection() {
   //randomly return either rock, paper, or scissors
-  //declare computerSelection variable
-  let computerSelection = "";
   //use Math.random to get 0, 1, or 2, use a conditional to assign 'rock', 'paper', or 'scissors' value to computerSelection based on random number
   computerSelection = Math.floor(Math.random() * 3);
   if (computerSelection === 0) {
@@ -14,55 +18,71 @@ function getComputerChoice() {
     computerSelection = "scissors";
   }
   //return computerSelection value
-  console.log(computerSelection);
   return computerSelection;
 }
 
-let computerSelection = getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+  //assign computerChoice to variable
+  computerSelection = getComputerSelection();
+  //use prompt() to get input from user
+  playerSelection = prompt("Rock, paper, or scissors?");
+  //playerSelection parameter should be case-insensitive, change user input to all lowercase to be able to match a computerSelection choice
+  playerSelection = playerSelection.toLowerCase();
 
-//use prompt() to get input from user
-let playerSelection = prompt("Rock, paper, or scissors?");
-//playerSelection parameter should be case-insensitive, change user input to all lowercase to be able to match a computerSelection choice
-playerSelection = playerSelection.toLowerCase();
-console.log(playerSelection);
-
-function playGame(playerSelection, computerSelection) {
   //return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-  //write conditional that determines winner
+  //write conditional that determines winner and alters scores
+
   if (playerSelection === "rock") {
     if (computerSelection === "rock") {
-      console.log(`It's a tie!`);
+      console.log(`Computer also plays rock. It's a tie this round!`);
     } else if (computerSelection === "paper") {
-      console.log("Computer plays paper. Computer wins!");
+      console.log("Computer plays paper. Computer wins this round!");
+      scores["computerScore"]++;
     } else if (computerSelection === "scissors") {
-      console.log("Computer plays scissors. You win!");
+      console.log("Computer plays scissors. You win this round!");
+      scores["playerScore"]++;
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "paper") {
-      console.log(`It's a tie!`);
+      console.log(`Computer also plays paper. It's a tie this round!`);
     } else if (computerSelection === "scissors") {
-      console.log("Computer plays scissors. Computer wins!");
+      console.log("Computer plays scissors. Computer wins this round!");
+      scores["computerScore"]++;
     } else if (computerSelection === "rock") {
-      console.log("Computer plays rock. You win!");
+      console.log("Computer plays rock. You win this round!");
+      scores["playerScore"]++;
     }
   } else if (playerSelection === "scissors") {
     if (computerSelection === "scissors") {
-      console.log(`It's a tie!`);
+      console.log(`Computer also plays scissors. It's a tie this round!`);
     } else if (computerSelection === "rock") {
-      console.log("Computer plays rock. Computer wins!");
+      console.log("Computer plays rock. Computer wins this round!");
+      scores["computerScore"]++;
     } else if (computerSelection === "paper") {
-      console.log("Computer plays paper. You win!");
+      console.log("Computer plays paper. You win this round!");
+      scores["playerScore"]++;
     }
+  }
+
+  return scores;
+}
+
+function playGame() {
+  //play 5 rounds, keep score, say winner at end
+
+  //do 5 times
+  for (let i = 0; i < 5; i++) {
+    playRound(playerSelection, computerSelection);
+  }
+
+  //console.log winner at end
+  if (scores["playerScore"] > scores["computerScore"]) {
+    console.log("You win!!!!!!!");
+  } else if (scores["playerScore"] < scores["computerScore"]) {
+    console.log("You lose!!!!!!!");
+  } else if (scores["playerScore"] === scores["computerScore"]) {
+    console.log(`It's a tie!!!!!!!!`);
   }
 }
 
-playGame(playerSelection, computerSelection);
-
-function game() {
-  //goes inside playGame function
-  //has 5 rounds
-  //console.log results after each round
-  //keeps score
-  //reports winner or loser at end
-  //console.log winner at end
-}
+playGame();
